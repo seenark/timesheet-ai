4d8be82faee604b2bebba35a3f8e0f03195b4f08
Atiwat Seenark <um_oom@hotmail.com>
2026-04-30 08:59:36 +0000

    feat: add Git webhook endpoint for push and PR events

--- apps/server/src/routes/index.ts
+++ apps/server/src/routes/index.ts
@@ -2,8 +2,10 @@ import { Elysia } from "elysia";
 import { eventRoutes } from "./events";
 import { healthRoutes } from "./health";
 import { integrationRoutes } from "./integrations";
+import { webhookRoutes } from "./webhooks";
 
 export const routes = new Elysia()
   .use(healthRoutes)
   .use(integrationRoutes)
-  .use(eventRoutes);
+  .use(eventRoutes)
+  .use(webhookRoutes);
