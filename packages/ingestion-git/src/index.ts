f662f97a05ccc5595fd4c7d176b92fa6b9101684
Atiwat Seenark <um_oom@hotmail.com>
2026-05-02 03:04:50 +0000

    feat(ingestion-git): add GitIngestionPlugin with local git sync

---
+++ packages/ingestion-git/src/index.ts
@@ -0,0 +1,5 @@
+export { extractGitIdentities } from "./identity-extractor";
+export { normalizeGitPayload } from "./normalizer";
+export { GitIngestionPlugin } from "./plugin";
+export { extractGitScopes } from "./scope-extractor";
+export type { GitCommitEnvelope, GitConfig } from "./types";
